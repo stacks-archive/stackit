b5ec1294869f6cc0e15ba9e0ca4d4590971d96f0
Sahana Srinivasan <sahanas009@gmail.com>
2019-07-16 18:02:00 -0400

    MVP landing page

---
+++ auth/src/Signin.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class Signin extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { handleSignIn } = this.props;
+
+    return (
+      <div className="panel-landing" id="section-1">
+        <h1 className="landing-heading">Stackit</h1>
+        <br/>
+        <button
+          className="btn btn-primary btn-lg"
+          onClick={ handleSignIn.bind(this) }
+        >
+          Sign in with Blockstack
+        </button>
+      </div>
+    );
+  }
+}
